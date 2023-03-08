7a50faa670fc7c9ebae9879ab99cbeb3c627afc9
Aditya Thakur <adityachauhan9456923436@gmail.com>
2023-03-09 01:25:19 +0530

    Flutter Upgrade and log out added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:chat_app/auth/register.dart';
 import 'package:chat_app/home.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -22,6 +23,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -34,9 +36,25 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const Register(),
+      home: MainPage(),
     );
   }
 }
 
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return Home();
+        } else {
+          return Register();
+        }
+      },
+    );
+  }
+}
