1340811633c34a9a195ebc0240fd1d5532645cbf
Hemendra Kumar <hem.pachahara@gmail.com>
2023-03-11 19:29:39 +0530

    free

--- lib/Chatroom.dart
+++ lib/Chatroom.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 
 import 'dart:developer';
-
 import 'package:chat_app/main.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -10,7 +9,6 @@ import 'package:flutter/material.dart';
 import 'package:chat_app/model/ChatRoomModel.dart';
 import 'package:chat_app/model/MessageModel.dart';
 import 'package:chat_app/model/UserModel.dart';
-
 import 'home.dart';
 
 class Chatroom extends StatefulWidget {
@@ -74,139 +72,145 @@ class _ChatroomState extends State<Chatroom> {
       ),
       body: SafeArea(
           child: SingleChildScrollView(
-        child: Column(
-          children: [
-            Expanded(
-              child: Container(
-                child: StreamBuilder(
-                  stream: FirebaseFirestore.instance
-                      .collection("chatrooms")
-                      .doc(widget.chatroomod.chatroomid)
-                      .collection("message")
-                      .snapshots(),
-                  // initialData: initialData,
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.active) {
-                      if (snapshot.hasData) {
-                        QuerySnapshot dataSnapshot =
-                            snapshot.data as QuerySnapshot;
+        child: SizedBox(
+          height: 600,
+          width: 550,
+          child: Column(
+            children: [
+              Expanded(
+                child: Container(
+                  // width: MediaQuery.of(context).size.width * 9,
+                  // height: MediaQuery.of(context).size.height * 9,
+                  child: StreamBuilder(
+                    stream: FirebaseFirestore.instance
+                        .collection("chatrooms")
+                        .doc(widget.chatroomod.chatroomid)
+                        .collection("message")
+                        .snapshots(),
+                    // initialData: initialData,
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.active) {
+                        if (snapshot.hasData) {
+                          QuerySnapshot dataSnapshot =
+                              snapshot.data as QuerySnapshot;
 
-                        return ListView.builder(
-                          reverse: true,
-                          itemCount: dataSnapshot.docs.length,
-                          itemBuilder: (context, index) {
-                            MessageModel currentMessage = MessageModel.fromMap(
-                                dataSnapshot.docs[index].data()
-                                    as Map<String, dynamic>);
+                          return ListView.builder(
+                            reverse: true,
+                            // shrinkWrap: true,
+                            physics: BouncingScrollPhysics(),
+                            itemCount: dataSnapshot.docs.length,
+                            itemBuilder: (context, index) {
+                              MessageModel currentMessage =
+                                  MessageModel.fromMap(dataSnapshot.docs[index]
+                                      .data() as Map<String, dynamic>);
 
-                            return Row(
-                              mainAxisAlignment:
-                                  (currentMessage.sender == widget.userm.uid)
-                                      ? MainAxisAlignment.end
-                                      : MainAxisAlignment.start,
-                              children: [
-                                Container(
-                                  margin: EdgeInsets.symmetric(vertical: 2),
-                                  padding: EdgeInsets.symmetric(
-                                      vertical: 10, horizontal: 10),
-                                  decoration: BoxDecoration(
-                                    color: (currentMessage.sender ==
-                                            widget.userm.uid)
-                                        ? Colors.grey
-                                        : Theme.of(context)
-                                            .colorScheme
-                                            .secondary,
-                                    borderRadius: BorderRadius.circular(5),
-                                  ),
-                                  child: Text(
-                                    currentMessage.text.toString(),
-                                    style: TextStyle(
-                                      color: Colors.white,
+                              return Row(
+                                mainAxisAlignment:
+                                    (currentMessage.sender == widget.userm.uid)
+                                        ? MainAxisAlignment.end
+                                        : MainAxisAlignment.start,
+                                children: [
+                                  Container(
+                                    margin: EdgeInsets.symmetric(vertical: 2),
+                                    padding: EdgeInsets.symmetric(
+                                        vertical: 10, horizontal: 10),
+                                    decoration: BoxDecoration(
+                                      color: (currentMessage.sender ==
+                                              widget.userm.uid)
+                                          ? Colors.grey
+                                          : Colors.amber,
+                                      borderRadius: BorderRadius.circular(5),
+                                    ),
+                                    child: Text(
+                                      currentMessage.text.toString(),
+                                      style: TextStyle(
+                                        color: Color.fromARGB(255, 235, 43, 43),
+                                      ),
                                     ),
                                   ),
-                                ),
-                              ],
-                            );
-                          },
-                        );
+                                ],
+                              );
+                            },
+                          );
+                        } else {
+                          return Text('No data available');
+                        }
                       } else {
-                        return Text('No data available');
+                        return Center(child: CircularProgressIndicator());
                       }
-                    } else {
-                      return Center(child: CircularProgressIndicator());
-                    }
-                  },
+                    },
+                  ),
                 ),
               ),
-            ),
 
-            // Text("${widget.userm}"),
-            // Text("${widget.targetuser}"),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Container(
-                height: 60.0,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(15.0),
-                      topRight: Radius.circular(15.0)),
-                ),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Container(
-                      width: MediaQuery.of(context).size.width * .9,
-                      height: MediaQuery.of(context).size.height * .1,
-                      child: TextFormField(
-                        cursorColor: Colors.green,
-                        autocorrect: true,
-                        controller: msgcontrol,
-                        textAlign: TextAlign.start,
-                        validator: (value) {
-                          if (msgcontrol.text.length > 0) {
-                            setState(() {
-                              _isTextFilled = true;
-                            });
-                          }
-                        },
-                        decoration: InputDecoration(
-                          labelStyle: TextStyle(
-                              color: Color.fromARGB(255, 81, 187, 104)),
-                          labelText: "enter message",
-                          suffixIcon: _isTextFilled
-                              ? IconButton(
-                                  onPressed: () {
-                                    sendmessage();
-                                  },
-                                  icon: const Icon(
-                                    Icons.send,
-                                    color: Color.fromARGB(255, 83, 212, 88),
-                                  ),
-                                )
-                              : null,
-                          border: const OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(18))),
-                          hintText: "Enter something to Chats ",
-                          focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(25.0),
+              // Text("${widget.userm}"),
+              // Text("${widget.targetuser}"),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  height: 60.0,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(15.0),
+                        topRight: Radius.circular(15.0)),
+                  ),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Container(
+                        width: MediaQuery.of(context).size.width * .9,
+                        height: MediaQuery.of(context).size.height * .1,
+                        child: TextFormField(
+                          cursorColor: Colors.green,
+                          autocorrect: true,
+                          controller: msgcontrol,
+                          textAlign: TextAlign.start,
+                          validator: (value) {
+                            // if (msgcontrol.text.length > 0) {
+                            //   setState(() {
+                            //     _isTextFilled = true;
+                            //   });
+                            // }
+                          },
+                          decoration: InputDecoration(
+                            labelStyle: TextStyle(
+                                color: Color.fromARGB(255, 81, 187, 104)),
+                            labelText: "enter message",
+                            suffixIcon: _isTextFilled
+                                ? IconButton(
+                                    onPressed: () {
+                                      sendmessage();
+                                    },
+                                    icon: const Icon(
+                                      Icons.send,
+                                      color: Color.fromARGB(255, 83, 212, 88),
+                                    ),
+                                  )
+                                : null,
+                            border: const OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(18))),
+                            hintText: "Enter something to Chats ",
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(25.0),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    IconButton(
-                      color: Color.fromARGB(255, 48, 220, 53),
-                      onPressed: () {},
-                      icon: Icon(Icons.attach_file),
-                    ),
+                      IconButton(
+                        color: Color.fromARGB(255, 48, 220, 53),
+                        onPressed: () {},
+                        icon: Icon(Icons.attach_file),
+                      ),
 
-                    SizedBox(width: 4),
-                    // if(tcontroller.text!=null)
-                  ],
+                      SizedBox(width: 4),
+                      // if(tcontroller.text!=null)
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       )),
     );
